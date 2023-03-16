61b6e6266374594b67e690e730a056dcf178e353
搬砖达人小王git-flow <461864051@qq.com>
2023-03-16 23:36:21 +0800

    base完成

--- main.js
+++ main.js
@@ -4,18 +4,29 @@
 const { BrowserWindow, app } = require('electron')
 const path = require('path')
 
-app.whenReady().then(() => {
+const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 600,
+    width: 300,
     height: 300,
     alwaysOnTop: true, // 窗口是否永远在别的窗口的上面
     x: 100,
-    y: 1000
+    y: 1030,
+    frame: false // 是否展示标题栏
   })
-
   // mainWindow.loadURL('https://www.houdunren.com')
-  // alert(999) //  alert is not defined
-
-  mainWindow.webContents.toggleDevTools()
+  // mainWindow.webContents.toggleDevTools() // 打开控制台
+  mainWindow.setAspectRatio(1) // 拖动窗口是拉伸宽高比例
   mainWindow.loadFile(path.resolve(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  createWindow()
 })
