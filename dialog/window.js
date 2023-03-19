d3e2348aaf55a95dfacafbcca8e792685ca74552
搬砖达人小王git-flow <461864051@qq.com>
2023-03-19 23:03:26 +0800

    对话框

---
+++ dialog/window.js
@@ -0,0 +1,29 @@
+const { BrowserWindow, shell } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 300,
+    alwaysOnTop: true,
+    x: 1500,
+    y: 100,
+    webPreferences: {
+      preload: path.resolve(__dirname, 'preload.js'),
+    },
+  })
+
+  win.webContents.openDevTools()
+  win.loadFile(path.resolve(__dirname, 'index.html'))
+
+  win.webContents.setWindowOpenHandler((details) => {
+    // 防止在开启新的窗口打开
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+  //var let const table  css3 js ->
+}
+
+module.exports = {
+  createWindow,
+}
